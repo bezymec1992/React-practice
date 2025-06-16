caa4458df93d7df67bc795e1a911a3df6bedade4
Vasiliy Lavrov <68432186+bezymec1992@users.noreply.github.com>
2025-06-16 22:52:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import CoupleMatch from './components/task1/CoupleMatch';
+import CoupleMatch from './components/Task1/CoupleMatch';
 import TranslateWorlds from './components/Task2/TranslateWords';
 
 function App() {
